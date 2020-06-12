d7312e88dc4d2f624398d4136c0f76df68e1531b
zhangzeli <zhangzeli@zhugeio.com>
2020-06-12 14:57:40 +0800

    m

---
+++ hexo/butterfly/js/myad.js
@@ -0,0 +1,27 @@
+(function(){
+    if(window.isMobile()){
+        return;
+    }
+    let body = $("body")
+    let content=$('#body-wrap')
+    let divleft = $("<div id='divleft'>广告位招租联系853089986@qq.com</div>")
+    let divright = $("<div id='divright'>广告位招租联系微信twobixiaoxin</div>")
+    content.append(divleft)
+    content.append(divright)
+    // console.log(content)
+    let navHeighe = $('#nav').height();
+    // console.log(navHeighe)
+    $(window).scroll(function(){
+        var scrollTopHeighet = $(this).scrollTop()
+        if(scrollTopHeighet>=navHeighe){
+            divright.css("display","inline")
+            divleft.css("display","inline")
+            divleft.css("top",(scrollTopHeighet)+"px")
+            divright.css("top",(scrollTopHeighet)+"px")
+        }else{
+            divright.css("display","none")
+            divleft.css("display","none")
+        }
+        // console.log(scrollTopHeighet-navHeighe)
+    })
+})(window)
